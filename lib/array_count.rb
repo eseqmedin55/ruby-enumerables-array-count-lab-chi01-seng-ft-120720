41a5652ea50e3736f7c0b414cc83a0b336f143e5
esequiel medina <medinaesequiel@gmail.com>
2020-12-01 19:00:46 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,9 +5,10 @@ end
 end
 
  def count_empty_strings(array)
+   empty_arrays = 0 
     counter = 0
 while counter < array.size
-array.count do |element|
+empty_arrays = array.count do |element|
   element.class == ""
 end
 counter += 1
